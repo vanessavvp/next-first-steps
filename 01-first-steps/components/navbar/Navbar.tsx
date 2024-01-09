5451ba2168c07570e0b225baa89796d27962d0c0
Vanessa Villalba <vanessavillalbap@gmail.com>
2024-01-09 20:37:35 +0000

    Add home icon to Navbar

--- 01-first-steps/components/navbar/Navbar.tsx
+++ 01-first-steps/components/navbar/Navbar.tsx
@@ -1,12 +1,34 @@
+import { HomeIcon } from "@primer/octicons-react"
+import Link from "next/link"
 
-export const Navbar = () => {
+interface NavItem {
+    path: string,
+    text: string
+}
+
+const navItems: Array<NavItem> = [
+    {
+        path: '/about', text: 'About'
+    }, {
+        path: '/pricing', text: 'Pricing'
+    }, {
+        path: '/contact', text: 'Contact'
+    }
+];
+
+export const Navbar = async () => {
     return (
         <nav className='flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded'>
-            <span>Home</span>
+            <Link className='flex items-center' href='/'>
+                <HomeIcon className='mr-2' />
+                <span>Home</span>
+            </Link>
             <div className='flex flex-1'></div>
-            <a className='mr-2' href='/about'>About</a>
-            <a className='mr-2' href='/pricing'>Pricing</a>
-            <a className='mr-2' href='/contact'>Contact</a>
-        </nav>
+            {
+                navItems.map(({ path, text }) => (
+                    <Link key={path} className='mr-2' href={path} > {text}</Link>
+                ))
+            }
+        </nav >
     )
 }
