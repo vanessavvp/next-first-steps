ea236625ceb933d15da0cc4c5d418fb72870ff41
Vanessa Villalba <vanessavillalbap@gmail.com>
2024-01-09 21:04:11 +0000

    Add ActiveLink client component and styles

---
+++ 01-first-steps/components/active-link/ActiveLink.tsx
@@ -0,0 +1,18 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+import style from './ActiveLink.module.css';
+
+interface Props {
+    path: string;
+    text: string;
+}
+
+export const ActiveLink = ({ path, text }: Props) => {
+    const pathName = usePathname();
+
+    return (
+        <Link className={`${style.link} ${(pathName == path) && style['active-link']}`} href={path}>{text}</Link>
+    )
+}
--- 01-first-steps/components/index.ts
+++ 01-first-steps/components/index.ts
@@ -1,1 +1,4 @@
 export * from './navbar/Navbar';
+
+// Client components
+export { ActiveLink } from './active-link/ActiveLink';
